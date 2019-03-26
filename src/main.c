8815ba514215e1be1c6c261a5aa8a9a453508ad7
HubCodes <krlrhkstk12@gmail.com>
2019-03-26 09:22:37 +0900

    change main.c; plain REPL

--- src/main.c
+++ src/main.c
@@ -1,7 +1,32 @@
+#include <error.h>
+#include <parse.h>
+
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello, world!\n");
+void init(void);
+void quit(void);
 
+int main(void) {
+  init();
+
+  for (;;) {
+    Error_init();
+    ASTNode* node = Parser_parseExpression();
+    if (Parser_isEOFNode(node)) {
+      break;
+    }
+    Error_clear();
+  }
+
+  quit();
+  
   return 0;
+}
+
+void init(void) {
+  Parser_init();
+}
+
+void quit(void) {
+  Error_clear();
 }
